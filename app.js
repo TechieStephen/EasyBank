3ecb9e8b49f13edfaf97f2d262502ffd135431bb
ohien stephen <ohienstephen@gmail.com>
2021-10-18 20:10:15 +0100

    -----

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const menu = document.getElementById("menu-bar")
+const menu = document.getElementById("menu")
+const menu_bar = document.getElementById("menu-bar")
 const nav = document.getElementById("nav-menu")
 
 menu.addEventListener('click', ()=>{
     nav.classList.toggle('toggle-nav')
     if(nav.classList.contains('toggle-nav')){
-        menu.style.transform = 'rotate(180deg)'
-        menu.src = "images/icon-close.svg"
+        menu_bar.src = "images/icon-close.svg"
     }else{
-    menu.src = "images/icon-hamburger.svg"
+        menu_bar.src = "images/icon-hamburger.svg"
     }
 })
